4329db11cf0c8854f0c18aeceb3de15f804d5690
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-05-13 19:55:22 -0300

    imp: Notas de aula 220513

--- src/main/java/br/com/hellospring/controller/HomeController.java
+++ src/main/java/br/com/hellospring/controller/HomeController.java
@@ -3,10 +3,12 @@ package br.com.hellospring.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@Controller possui o mesmo comportamento que a servlet, conectando o cliente ao servidor
+@RestController // abstrai algumas annotations auxiliares para exibição de informação na tela
 public class HomeController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/") // mapeia o endpoint (identificação da funcionalidade na url) que será usado na aplicação
+	// @ResponseBody - usado em conjunto com o @Controller para exibição de informações na tela
 	public String homeMessage() {
 		return "Hello World!";
 	}
